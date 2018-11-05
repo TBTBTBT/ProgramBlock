10d1f02375159c56acd3f885648d4a3214b4c49c
Shogo Tabata <>
2018-11-06 00:02:08 +0900

    riset

---
+++ Assets/Toast/Masterdata/MasterTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MasterTable<T> where T : class, IMasterRecord
+{
+    public Master<T> Records { get; private set; } //静的に確保
+    public static MasterTable<T> _instance;
+    public static MasterTable<T> Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new MasterTable<T>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public T Get(int id)
+    {
+        return Records.Records.FirstOrDefault(_ => _.id == id);
+    }
+    public void Init(string path)
+    {
+        string assetPath = Application.streamingAssetsPath + path;
+        Debug.Log("[Master] load path : " + assetPath);
+        string text = System.IO.File.ReadAllText(assetPath);
+        // Debug.Log(text);
+        Records = JsonUtility.FromJson<Master<T>>(text);
+        //Debug.Log(Records.Records);
+    }
+}
+
+public interface IMasterRecord
+{
+    int id { get; set; }
+
+}
+
+[Serializable]
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class MasterPath : Attribute
+{
+    public string Path;
+
+    public MasterPath(string path)
+    {
+        Path = path;
+    }
+}
+[Serializable]
+public class Master<T> where T:class, IMasterRecord{
+    public T[] Records;
+}
---
+++ Assets/Toast/Masterdata/MasterdataManager.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class MasterdataManager : SingletonMonoBehaviour<MasterdataManager>
+{
+ //   Dictionary<Type,MasterTable<>>
+    public static T Get<T>(int id) where T : class,IMasterRecord
+    {
+        return MasterTable<T>.Instance.Get(id);
+        /*
+        if (typeof(T) == typeof(MstBulletRecord))
+        {
+            return (T)(object)MstBulletRecordTable.First(_ => _.id == id);
+        }
+        return null;*/
+    }
+    /*
+    public static MstBulletRecord[] MstBulletRecordTable = {
+        new MstBulletRecord(){id = 10,gravy = -20f,gravx = 0.95f,rad = 10,weight = 1f}
+    };*/
+    /*
+    protected override void Awake()
+    {
+
+        base.Awake();
+        InitMasterdata(FindMasterdata());
+    }
+    */
+
+    //initialize
+    void InitRecord<T>() where T:class,IMasterRecord
+    {
+        Attribute[] attributes = Attribute.GetCustomAttributes(typeof(T), typeof(MasterPath));
+        //object[] attributes = typeof(T).GetCustomAttributes(typeof(MasterPath),false);
+        if (attributes == null || attributes.Length == 0)
+        {
+            throw new InvalidOperationException("The provided object is not serializable");
+           
+        }
+        MasterPath path = attributes[0] as MasterPath;
+        if (path == null)
+        {
+            return;
+        }
+       // Debug.Log(typeof(T) + "Init");
+        MasterTable<T>.Instance.Init(path.Path);
+    }
+    static Type[] GetInterfaces<T>()
+    {
+        return Assembly.GetExecutingAssembly().GetTypes().Where(c => c.GetInterfaces().Any(t => t == typeof(T))).ToArray();
+    }
+    Type[] FindMasterdata()
+    {
+        var masterDataList = GetInterfaces<IMasterRecord>();
+        Debug.Log(masterDataList.Length);
+        return masterDataList;
+    }
+    public IEnumerator InitMasterdataAsync()
+    {
+        var masterDataList = FindMasterdata();
+        Debug.Log("Listup Masterdata Class.");
+        yield return null;
+        var method = typeof(MasterdataManager).GetMethod("InitRecord", BindingFlags.Public |
+                                                          BindingFlags.NonPublic |
+                                                          BindingFlags.Instance |
+                                                          BindingFlags.Static |
+                                                          BindingFlags.DeclaredOnly);
+        if (method == null)
+        {
+            Debug.Log("null");
+            yield break;
+
+        }
+        Debug.Log("GetMethod.");
+        yield return null;
+        foreach (var type in masterDataList)
+        {
+            var generic = method.MakeGenericMethod(type);
+            generic.Invoke(this, null);
+            yield return null;
+            // InitRecord<>();
+        }
+        Debug.Log("Initialized.");
+    }
+
+    void InitMasterdata(Type[] masterDataList)
+    {
+        var method = typeof(MasterdataManager).GetMethod("InitRecord",BindingFlags.Public |
+                                                          BindingFlags.NonPublic |
+                                                          BindingFlags.Instance |
+                                                          BindingFlags.Static |
+                                                          BindingFlags.DeclaredOnly);
+        if (method == null)
+        {
+            Debug.Log("null");
+            return;
+
+        }
+        
+        foreach (var type in masterDataList)
+        {
+            var generic = method.MakeGenericMethod(type);
+            generic.Invoke(this, null);
+           // InitRecord<>();
+        }
+        
+    }
+}
