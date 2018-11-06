2fd9fbd2e93bc1adc9f0af357272b64863e0c886
田畑　翔伍 <shogo.tabata@sega.com>
2018-11-06 19:36:15 +0900

    新規

---
+++ Assets/Script/Modules/Kernel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kernel: MonoBehaviourWithStatemachine<Kernel.State>
+{
+    public enum State
+    {
+        Boot,
+        Init,
+
+
+    }
+
+    private MachineCore _machine;
+    private List<ModuleBase> _modules;
+
+    private List<ProcessorBase> _processores;
+
+    IEnumerator Boot()
+    {
+        //カーネル初期化
+        //ハードウエア初期化
+        yield return null;
+    }
+
+    IEnumerator Init()
+    {
+        //プロセッサ初期化
+        //モジュール初期化
+        yield return null;
+    }
+    //ConnectModule
+    //RemoveModule
+    //
+}
+//heatX
+//hp
+//
