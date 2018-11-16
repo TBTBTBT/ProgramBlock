06866235d79394dd1200c54ed97397c76b414ce2
田畑　翔伍 <shogo.tabata@sega.com>
2018-11-16 17:31:11 +0900

    途中

--- Assets/Script/Utility/EndlessScroller.cs
+++ Assets/Script/Utility/EndlessScroller.cs
@@ -7,9 +7,26 @@ using UnityEngine.UI;
 
 public class EndlessScroller : UIBehaviour
 {
+    [System.Serializable]
+    enum Direction
+    {
+        Horizontal,
+        Vertical
+    }
+    [SerializeField]
+    private GameObject _itemPrefab;
 
+    [SerializeField]
+    private Direction _direction;
+
+    [SerializeField]
+    private RectTransform _scrollContents;
     [System.Serializable]
     public class OnUpdate : UnityEngine.Events.UnityEvent<int, GameObject> { }
     public OnUpdate OnItemUpdate = new OnUpdate();
+    //Contentsのサイズを調整
+    void SetContentsSize()
+    {
 
+    }
 }
